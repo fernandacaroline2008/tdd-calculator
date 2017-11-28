f0d98b0d994ba955513a1570b91432f5576edade
Fernanda Caroline Santos <fcsantos@fitec.org.br>
2017-11-28 13:09:07 -0200

    Add serialVersionUID

--- src/br/org/fitec/cpi/tdd/ex1/NegativeNumberException.java
+++ src/br/org/fitec/cpi/tdd/ex1/NegativeNumberException.java
@@ -5,6 +5,10 @@ import java.util.stream.Collectors;
 
 public class NegativeNumberException extends Exception {
 
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
     private static final String SEP = " ";
     private static final String MESSAGE = "negatives not allowed: ";
 
